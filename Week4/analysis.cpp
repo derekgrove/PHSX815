09ffdf17aee640242229ffb30b37f7f9ea96f91c
derekgrove <dgrove241@gmail.com>
2023-05-09 13:34:09 -0500

    fixed plotting
    I know how to use ROOT now, didn't know ROOT back in the day (Project 1 era.)

--- Week4/analysis.cpp
+++ Week4/analysis.cpp
@@ -1,50 +1,55 @@
-#include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
 #include <vector>
-#include "gnuplot.hpp"
 
-int main() {
-    std::ifstream infile;
-    // attempt to open the file
-    infile.open("data2.txt");
-    if (!infile) {
-        std::cerr << "Failed to open input file." << std::endl;
-        return 1;
+void analysis() {
+
+    // Create a 1D histogram with 12 bins from 0 to 12.
+    TH1F *hist = new TH1F("hist", "Particle Distribution from e+e- collision;Value;Frequency", 12, 0, 12);
+
+    // Define the bin labels.
+    std::vector<std::string> product_names = {"#nu_{e}", "#nu_{#mu}", "#nu_{#tau}", "e", "u", "d", "s",
+                                          "#mu", "c", "#tau", "b", "t"};
+
+    // Set the bin labels.
+    for (size_t i = 0; i < product_names.size(); ++i) {
+        hist->GetXaxis()->SetBinLabel(i + 1, product_names[i].c_str());
     }
 
-    // verify that the file actually opened...
-    std::vector<int> results;
-    if (infile.is_open())
-    {
-        int num;
-        while(infile >> num) {
-            results.push_back(num);
+    // Open the .txt file.
+    std::ifstream file("data.txt");
+    std::string line;
+
+    // Check if the file is open.
+    if (file.is_open()) {
+        // Read the file line by line.
+        while (getline(file, line)) {
+            // Use a string stream to split the line into separated strings.
+            std::istringstream ss(line);
+            std::string value;
+
+            // Process each string in the line.
+            while (getline(ss, value, ' ')) {
+                // Convert the string to an integer.
+                int i = std::stoi(value);
+
+                // Fill the histogram with the integer.
+                hist->Fill(i);
+            }
         }
 
-        infile.close();
+        // Close the file.
+        file.close();
+    } else {
+        std::cout << "Unable to open the file." << std::endl;
+        return;
     }
-    //for (int i = 0; i < results.size(); i++) {
-    //    std::cout << results[i] << " ";
-    //}
-    //std::cout << std::endl;
 
-    Gnuplot gp;
+    // Create a canvas to draw the histogram.
+    TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
+    hist->Draw();
 
-    // set the style of the histogram
-    gp.set_style("histogram");
-
-    // set the title and labels of the histogram
-    gp.set_title("Example Histogram");
-    gp.set_xlabel("Value");
-    gp.set_ylabel("Frequency");
-
-    // create a vector of data to plot
-    std::vector<double> data = {1, 2, 3, 4, 5, 5, 5, 6, 6, 6, 6};
-
-    // plot the data as a histogram
-    gp.plot_histogram(data, "Example");
-
-    //std::cin.get(); // to pause the program
-
-    return 0;
+    // Save the histogram as a .png file.
+    c1->SaveAs("histogram.png");
 }
